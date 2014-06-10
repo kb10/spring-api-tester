e442b7da57e985fbaa9479862c7e1b35d67b4cfd
ruben malchow <ruben.malchow@gmail.com>
2014-06-10 17:35:17 +0800

    removed node modules. added initial angular app config

--- apitester-webapp/src/main/webapp/apitester/app/src/js/app.js
+++ apitester-webapp/src/main/webapp/apitester/app/src/js/app.js
@@ -1,2 +1,8 @@
-	var angular = angular;
-	var apitester = angular.module('apitester', ['angularMoment']);
+var angular = angular;
+var apitester = angular.module('fgApitester', ['restangular']);
+
+apitester.controller('rootController', function($scope) {
+
+	$scope.name = 'enter your name';
+
+})
