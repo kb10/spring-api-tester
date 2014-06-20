39bcef5d56044985821d1947c1b5b76bc89d826b
ruben malchow <ruben.malchow@gmail.com>
2014-06-20 10:43:17 +0800

    some stuff

--- apitester-webapp/src/main/webapp/apitester/app/src/js/app.js
+++ apitester-webapp/src/main/webapp/apitester/app/src/js/app.js
@@ -1,8 +1,20 @@
 var angular = angular;
-var apitester = angular.module('fgApitester', ['restangular']);
+var apitester = angular.module('fgApitester', ['restangular']).config(
+	function(RestangularProvider) {
+		RestangularProvider.setBaseUrl('api');
+	}
+);
 
-apitester.controller('rootController', function($scope) {
 
-	$scope.name = 'enter your names';
 
-})
+apitester.controller('rootController', [ '$scope' , 'Restangular', function($scope, RA) {
+
+	RA.all('basepaths').getList().then(
+		function(basepaths) {
+			$scope.basepaths = basepaths;
+			console.log($scope.basepaths);
+		}
+	);
+
+
+}]);
