be41bde93481d582b5d55b2634d509607c4ea8c1
ruben malchow <ruben.malchow@gmail.com>
2014-07-07 14:53:47 +0800

    configuration stuff

---
+++ apitester-webapp-example/src/main/java/com/cinefms/apitester/mytests/controllers/TestController8.java
@@ -0,0 +1,17 @@
+package com.cinefms.apitester.mytests.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RequestMapping(value={"/aaa/{aaaId}"})
+public class TestController8 {
+
+	@RequestMapping(value={"/x/{id}"},method=RequestMethod.GET)
+	@ResponseBody
+	public String getBasic() {
+		return null;
+	}
+	
+}
