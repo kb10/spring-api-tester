6ea8b04b4d12527084f9090cd2ff33fbd95ca66c
tz250301 <tom.zhang@crm-factory.com.cn>
2014-07-18 12:04:18 +0800

    add indexedDB

--- apitester-webapp/src/main/webapp/apitester/app/src/js/test_controllers.js
+++ apitester-webapp/src/main/webapp/apitester/app/src/js/test_controllers.js
@@ -1,5 +1,10 @@
-apitester.controller('testRootController', [ '$scope' , '$http', '$interval','Restangular',
- function($scope, $http, $interval, RA) {
+apitester.controller('testRootController', [ '$scope' , '$http', '$interval','Restangular', '$indexedDB',
+ function($scope, $http, $interval, RA, $indexedDB) {
+
+ 	// var historyStore = $indexedDB.objectStore('history');
+ 	// historyStore.insert({"id": "1","name": "test"}).then(function(e){
+ 	// 	console.log(e.message);
+ 	// });
 
 	$scope.requestConfig = {};
 	$scope.selectedCallInfo = {};
@@ -173,8 +178,10 @@ apitester.controller('testRootController', [ '$scope' , '$http', '$interval','Re
 	};
 
 	$scope.prepareRequest = function() {
-		var serverBaseUrl = 'http://127.0.0.1:8080';
-		var requestUrl = serverBaseUrl + $scope.selectedCallInfo.fullPath;
+		// var serverBaseUrl = 'http://127.0.0.1:8080';
+		// var serverBaseUrl = 'http://de-dev.fairgarage.de';
+		// var requestUrl = serverBaseUrl + $scope.selectedCallInfo.fullPath;
+		var requestUrl = $scope.selectedCallInfo.fullPath;
 		if($scope.selectedCallInfo.pathParameters.length > 0) {
 			for(i = 0; i < $scope.selectedCallInfo.pathParameters.length; i++) {
 				requestUrl = requestUrl.replace("{" + $scope.selectedCallInfo.pathParameters[i].parameterName + "}", 
