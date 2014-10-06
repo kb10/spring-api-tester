de99898527c2c256aebb965cae525fcbe3fcfd75
ruben malchow <ruben.malchow@gmail.com>
2014-10-06 20:14:47 +0200

    param structure nicer now

--- apitester-webapp/src/main/webapp/apitester/app/src/js/docdetail_controller.js
+++ apitester-webapp/src/main/webapp/apitester/app/src/js/docdetail_controller.js
@@ -23,6 +23,33 @@ apitester.controller('docDetailController', ['$scope','$http','$location', funct
 		    pos = absUrl.lastIndexOf('apitester');
 		return absUrl.substring(0, pos + 9);
 	};
+
+	$scope.showParamDetails = function(){
+
+		$scope.returnParamExpanded = !$scope.returnParamExpanded;
+		
+		if($scope.apicall.returnType.returnClass.className.indexOf('java.') >= 0) {
+			return;
+		}
+
+		var baseUrl = $scope.getBaseUrl();
+		$scope.docparam = {
+			detail:'"{none}"',
+			showDetail:false
+		};		
+		url = baseUrl + '/api/objects/'+$scope.apicall.returnType.returnClass.className+'/details';
+
+		$http({	method : 'GET',
+			url : url,
+			params : '',
+			data : ''}). 
+		success(function(data, status, headers, config, statusText) {
+			$scope.returnParamDetails = angular.toJson(data, true);
+		}).error(function(data, status, headers, config, statusText) {
+			$scope.returnParamDetails = 'ERROR happened!';
+		});
+	};
+
  }]);
 
 apitester.controller('ParamController', ['$scope','$http','$location', function($scope, $http, $location) {
